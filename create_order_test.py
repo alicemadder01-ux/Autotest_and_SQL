261c9f7c02cf33728e91a8ac215661a306778d79
alicemadder01-ux <alicemadder01@gmail.com>
2026-06-08 12:50:51 +0400

    Add files via upload

---
+++ create_order_test.py
@@ -0,0 +1,16 @@
+import sender_stand_request
+import data
+
+
+def test_order_creation_and_retrieval():
+    response = sender_stand_request.create_order(data.order_body)
+
+    track_number = response.json()["track"]
+    print("Заказ создан. Номер трека:", track_number)
+
+    order_response = sender_stand_request.get_order(track_number)
+
+    assert order_response.status_code == 200, f"Ошибка: {order_response.status_code}"
+    order_data = order_response.json()
+    print("Данные заказа:")
+    print(order_data)
